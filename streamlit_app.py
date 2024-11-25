626954a675d623f3a3145943797a2e787974a4ff
AmeedShaik <57082685+AmeedShaik@users.noreply.github.com>
2024-11-25 23:09:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
 
 # Title of the app
 st.title("Stock Price Prediction App")
