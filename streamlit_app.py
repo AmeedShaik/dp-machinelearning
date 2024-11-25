a31945d063000a3550b6ba959ae312e4cacc08dc
AmeedShaik <57082685+AmeedShaik@users.noreply.github.com>
2024-11-26 00:33:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ stock_ticker = st.sidebar.text_input("Enter Stock Ticker (e.g., AAPL, TSLA, INFY
 start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2015-01-01"))
 end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2023-01-01"))
 # Load the model
-st.cache_data(allow_output_mutation=True)
+st.cache_resource(allow_output_mutation=True)
 def load_lstm_model():
     return load_model("stock_prediction_lstm.h5.py")  # Make sure your trained model is in the same directory.
