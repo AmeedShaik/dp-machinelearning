e0d7c22ceaf9db3fda9ff3061b36cda37313e580
AmeedShaik <57082685+AmeedShaik@users.noreply.github.com>
2024-11-25 23:08:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,3 @@ import matplotlib.pyplot as plt
 
 # Title of the app
 st.title("Stock Price Prediction App")
-
-# Sidebar for user input
-st.sidebar.header("Stock Prediction Settings")
-
-# Select stock ticker
-stock_ticker = st.sidebar.text_input("Enter Stock Ticker (e.g., AAPL, TSLA, INFY.BO)", value="YESBANK.NS")
-
-# Select date range
-start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2015-01-01"))
-end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2023-01-01"))
