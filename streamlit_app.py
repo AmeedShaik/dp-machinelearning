76e946dbc75a904cc207e24ed88bbdc9dad45d14
AmeedShaik <57082685+AmeedShaik@users.noreply.github.com>
2024-11-25 23:38:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import MinMaxScaler
-import load_model
-import matplotlib as plt
 
 # Title of the app
 st.title("Stock Price Prediction Application")
