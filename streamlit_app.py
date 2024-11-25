70c5296a25611180b2bbabc3c12dbd9544cb79f5
AmeedShaik <57082685+AmeedShaik@users.noreply.github.com>
2024-11-25 23:20:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 
 st.title("Stock Price Prediction App")
